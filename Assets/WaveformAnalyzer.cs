3a8f36fae61280d5ac6eb6e8320b9b67e83cff0f
Reguluz <qc_2838d50e94ad4ea1b0097cece7e72de4@git.code.tencent.com>
2020-12-27 16:23:17 +0800

    Now mono clips can be renderered correctly, and remove some old scripts

--- Assets/WaveformAnalyzer.cs
+++ Assets/WaveformAnalyzer.cs
@@ -18,7 +18,7 @@ struct WaveformBlock
 [RequireComponent(typeof(AudioSource))]
 public class WaveformAnalyzer : MonoBehaviour
 {
-    [Range(0,4410)]public int Count;
+    [Range(5,12)]public int CountLevel;
     [Space] 
     [SerializeField] ComputeShader cs = default;
     [SerializeField] Material material = default;
@@ -27,8 +27,11 @@ public class WaveformAnalyzer : MonoBehaviour
     private ComputeBuffer _waveformOutput;
     private AudioSource _audioSource;
     private NativeArray<WaveformTick> _dbList;
-    private static int maxCount = 4410;
-
+    private static readonly int MAXCount = 4096;
+    private float[] _samples;
+    private List<float> _totalSampleList;
+    private int _count;
+    private List<float> _samplesClip;
     private static readonly int
         waveformTicksId = Shader.PropertyToID("_WaveformTicks"),
         waveformOutputId = Shader.PropertyToID("_WaveformOutputs"),
@@ -36,18 +39,24 @@ public class WaveformAnalyzer : MonoBehaviour
     private void OnEnable()
     {
         _audioSource = GetComponent<AudioSource>();
-        _dbList = new NativeArray<WaveformTick>(maxCount, Allocator.Persistent);
-        _waveformBuffer = new ComputeBuffer(maxCount, Marshal.SizeOf(typeof(WaveformTick)));
-        _waveformOutput = new ComputeBuffer(maxCount, Marshal.SizeOf(typeof(WaveformBlock)));
+        _dbList = new NativeArray<WaveformTick>(MAXCount, Allocator.Persistent);
+        _waveformBuffer = new ComputeBuffer(MAXCount, Marshal.SizeOf(typeof(WaveformTick)));
+        _waveformOutput = new ComputeBuffer(MAXCount, Marshal.SizeOf(typeof(WaveformBlock)));
+        _samples = new float[_audioSource.clip.samples];
+        AudioClip clip;
+        (clip = _audioSource.clip).GetData(_samples, 0);
+        _totalSampleList = new List<float>(_samples);
+        _samplesClip = new List<float>();
     }
 
     private void Update()
     {
+        _count = (int)Mathf.Pow(2, CountLevel);
         RefreshBuffer();
         RefreshShader();
         material.SetBuffer(waveformOutputId, _waveformOutput);
         var bounds = new Bounds(Vector3.zero, Vector3.one / 10);
-        Graphics.DrawMeshInstancedProcedural(mesh, 0, material, bounds, Count);
+        Graphics.DrawMeshInstancedProcedural(mesh, 0, material, bounds, _count);
 
     }
 
@@ -62,13 +71,30 @@ public class WaveformAnalyzer : MonoBehaviour
 
     void RefreshBuffer()
     {
-        float intervalTime = _audioSource.time;
-        float[] samples = new float[441000];
-        _audioSource.clip.GetData(samples, (int)(intervalTime * 44100));
-        for (int i = 0; i < maxCount; i++)
+        //当前起始位置时间
+        float currentTime = _audioSource.time;
+        //起始位置的采样序号
+        int currentSample = (int)(currentTime * 44100);
+        //获取往后的40960（或更少）的采样
+        int sampleClipLength = Mathf.Min(_samples.Length - currentSample, MAXCount * 10);
+        //原曲采样转换成列表
+        
+        _samplesClip.Clear();
+        //原曲采样截取至截取列表
+        _samplesClip = _totalSampleList.GetRange((int)(currentTime * 44100), sampleClipLength);
+        _samplesClip[0] = 0;
+        //list补全至全长
+        while (_samplesClip.Count < MAXCount * 10)
+        {
+            _samplesClip.Add(0);
+        }
+
+        int reverseLevel = (int)Mathf.Pow(2, (12 - CountLevel));
+        // Debug.Log(reverseLevel + " "+_count);
+        for (int i = 0; i < _count; i++)
         {
             var intervalDb = _dbList[i];
-            intervalDb.loudness = samples[100 * i];
+            intervalDb.loudness = _samplesClip[reverseLevel * i * 10];
             // intervalDb.time = intervalTime + (float)(10 * i) / 44100;
             _dbList[i] = intervalDb;
         }
@@ -81,7 +107,7 @@ public class WaveformAnalyzer : MonoBehaviour
         cs.SetFloat(TimeId, _audioSource.time);
         cs.SetBuffer(kernal, waveformTicksId, _waveformBuffer);
         cs.SetBuffer(kernal, waveformOutputId, _waveformOutput);
-        int groups = Mathf.CeilToInt(maxCount / 64f);
+        int groups = Mathf.CeilToInt(MAXCount / 64f);
         cs.Dispatch(kernal, groups, 1, 1);
         
     }
